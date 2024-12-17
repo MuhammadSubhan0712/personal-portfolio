31903fc8e5b418a4312c0ad481acfc6307c138f1
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-12-18 03:49:28 +0500

    Navbar.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -21,7 +21,9 @@ const Navbar = () => {
   }, []);
 
   return (
-    <div className="fixed top-12 right-6 mx-auto flex flex-col gap-2.5 items-end z-50">
+    <div className="fixed top-12 right-6 mx-auto flex 
+    flex-col gap-2.5 items-end z-50 md:right-auto 
+    md:left-1/2 md:translate-x-1/2">
       <button
         onClick={() => setIsOpen((preVal) => !preVal)}
         className="bg-background card-shadow p-3 md:hidden rounded">
@@ -42,13 +44,14 @@ const Navbar = () => {
           "bg-background card-shadow p-3 rounded md:block duration-300 ease-in-out",
           {
             "opacity-100": isOpen,
-            "opacity-0": !isOpen,
+            "opacity-0 md:opacity-100": !isOpen,
           }
         )}>
-        <ul>
+        <ul className="flex flex-col items-center gap-4 
+        text-lg font-normal md:flex-row">
           <li>
             <div
-              className={cn("rounded p-1  hover:cursor-pointer", {
+              className={cn("rounded p-1 duration-300 ease-in-out  hover:cursor-pointer", {
                 "bg-primary text-white": activeSection === "home",
               })}
               onClick={() => {
@@ -62,7 +65,7 @@ const Navbar = () => {
 
           <li>
             <div
-              className={cn("rounded p-1  hover:cursor-pointer", {
+              className={cn("rounded p-1 duration-300 ease-in-out  hover:cursor-pointer", {
                 "bg-primary text-white": activeSection === "skills",
               })}
               onClick={() => {
@@ -76,7 +79,7 @@ const Navbar = () => {
 
           <li>
             <div
-              className={cn("rounded p-1 ", {
+              className={cn("rounded p-1 duration-300 ease-in-out ", {
                 "bg-primary text-white": activeSection === "projects",
               })}
               onClick={() => {
@@ -90,7 +93,7 @@ const Navbar = () => {
 
           <li>
             <div
-              className={cn("rounded p-1  hover:cursor-pointer", {
+              className={cn("rounded p-1 duration-300 ease-in-out  hover:cursor-pointer", {
                 "bg-primary text-white": activeSection === "testimonials",
               })}
               onClick={() => {
@@ -104,7 +107,7 @@ const Navbar = () => {
 
           <li>
             <div
-              className={cn("rounded p-1 hover:cursor-pointer", {
+              className={cn("rounded p-1 duration-300 ease-in-out hover:cursor-pointer", {
                 "bg-primary text-white": activeSection === "experience",
               })}
               onClick={() => {
@@ -118,7 +121,7 @@ const Navbar = () => {
 
           <li>
             <div
-              className={cn("rounded p-1  hover:cursor-pointer", {
+              className={cn("rounded p-1 whitespace-nowrap duration-300 ease hover:cursor-pointer", {
                 "bg-primary text-white": activeSection === "contact",
               })}
               onClick={() => {
