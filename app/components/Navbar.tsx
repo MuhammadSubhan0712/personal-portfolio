f61e0bfb9f141655f27393bb8b9f6c0bead99014
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-12-18 01:38:41 +0500

    Working..

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,47 @@
-import React from 'react'
+"use client"
+import React, { useCallback, useEffect, useState } from 'react'
+import cn from "classNames"
 
-const Navbar = (props: Props) => {
+const Navbar = () => {
+
+  const [activeSection , setActiveSection] = useState("home");
+
+ useEffect(() => {
+    setActiveSection("home")
+ },[]);
+
+
+ const scrollToSection = useCallback
+ ((sectionId: string) => {
+  const section = document.getElementById(sectionId);
+
+  if (section) {
+    section.scrollIntoView({
+        behavior: "smooth",
+    });
+  }
+ } ,[]);
+
+
   return (
-    <div>Navbar</div>
+    <div className='fixed top-12 right-6 mx-auto flex flex-col gap-2.5 items-end z-50'>
+        <button className='bg-background card-shadow p-3 md:hidden rounded'>
+            <img className='block dark:hidden' src="/public/next.svg" alt="menu icon" />
+            <img className='block dark:block' src="/public/next.svg" alt="menu icon" />
+        </button>
+
+        <nav>
+            <ul>
+                <li>
+                    <div className={cn("rounded p-1 " , {
+                        "bg-primary text-white" :true
+                    })}>
+                        Home
+                    </div>
+                </li>
+            </ul>
+        </nav>
+    </div>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     <h1>H e l l o W o r l d</h1>
+    <div className="relative overflow-clip" id="home">
+     <Navbar/>
     </div>
   );
 }
