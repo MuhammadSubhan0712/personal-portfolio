252c62a6b966aa90abe667c94c2eda16eca15f87
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-12-18 01:56:34 +0500

    Working..

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,48 +1,65 @@
-"use client"
-import React, { useCallback, useEffect, useState } from 'react'
-import cn from "classNames"
+"use client";
+import React, { useCallback, useEffect, useState } from "react";
+import cn from "classnames";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [activeSection, setActiveSection] = useState("home");
 
-  const [activeSection , setActiveSection] = useState("home");
+  useEffect(() => {
+    setActiveSection("home");
+  }, []);
 
- useEffect(() => {
-    setActiveSection("home")
- },[]);
+  const scrollToSection = useCallback((sectionId: string) => {
+    const section = document.getElementById(sectionId);
 
-
- const scrollToSection = useCallback
- ((sectionId: string) => {
-  const section = document.getElementById(sectionId);
-
-  if (section) {
-    section.scrollIntoView({
+    if (section) {
+      section.scrollIntoView({
         behavior: "smooth",
-    });
-  }
- } ,[]);
-
+      });
+    }
+  }, []);
 
   return (
-    <div className='fixed top-12 right-6 mx-auto flex flex-col gap-2.5 items-end z-50'>
-        <button className='bg-background card-shadow p-3 md:hidden rounded'>
-            <img className='block dark:hidden' src="/public/next.svg" alt="menu icon" />
-            <img className='block dark:block' src="/public/next.svg" alt="menu icon" />
-        </button>
+    <div className="fixed top-12 right-6 mx-auto flex flex-col gap-2.5 items-end z-50">
+      <button
+        onClick={() => setIsOpen((preVal) => !preVal)}
+        className="bg-background card-shadow p-3 md:hidden rounded">
+        <img
+          className="block dark:hidden"
+          src="/public/next.svg"
+          alt="menu icon"
+        />
+        <img
+          className="block dark:block"
+          src="/public/next.svg"
+          alt="menu icon"
+        />
+      </button>
 
-        <nav>
-            <ul>
-                <li>
-                    <div className={cn("rounded p-1 " , {
-                        "bg-primary text-white" :true
-                    })}>
-                        Home
-                    </div>
-                </li>
-            </ul>
-        </nav>
+      <nav
+        className={cn("bg-background card-shadow p-3 rounded md:block duration-300 ease-in-out", {
+          "opacity-100": isOpen,
+          "opacity-0": isOpen,
+        })}>
+        <ul>
+          <li>
+            <div
+              className={cn("rounded p-1 ", {
+                "bg-primary text-white": true,
+              })}
+              onClick={() => {
+                setActiveSection("home");
+                scrollToSection("home");
+                setIsOpen(false);
+              }}>
+              Home
+            </div>
+          </li>
+        </ul>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
