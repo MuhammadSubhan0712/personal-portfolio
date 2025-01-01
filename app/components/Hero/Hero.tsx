d73483ec09542c9b2d2e287819732e08235dbb50
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-01-02 03:07:45 +0500

    Working..

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -1,23 +1,41 @@
-import React from 'react'
-
+import React from "react";
+import Image from "next/image";
 const Hero = () => {
   return (
-    <section className='relative flex flex-row items-center z-20 mx-auto'>
-     <div className="flex flex-col gap-[13px] items-center">
-        <h1 className='text-2xl/1 text-center font-semibold block'>
-        <span className='-ml-3 '></span>
-        Hello I'm Muhammad Subhan Khan, I'm 
-        <span className='block text-[27px] hightlight mt-2'>
-         Tech Enthusiast </span>
+    <section className="relative flex flex-row items-center z-20 mx-auto md:gap[37px] md:mx-10">
+      <div className="flex flex-col gap-[13px] items-center md:gap-[34px] md:my-[58px]">
+        <h1 className="text-2xl/1 text-center font-semibold block md:text-[40px] md:text-start md:inline">
+          <span className="-ml-3 "></span>
+          Hello I'm Muhammad Subhan Khan, I'm
+          <span className="block text-[27px] hightlight mt-2 md:text-[45px] md:inline md:mt-0">
+            {" "}
+            Tech Enthusiast{" "}
+          </span>
         </h1>
-        <p className='text-center text-sm mx-6'>
-        I'm obsessed with code and helping startups create unique and helpful products.
+        <p className="text-center text-sm mx-6 md:text-[22px] md:text-start md:mx-0">
+          I'm obsessed with code and helping startups create unique and helpful
+          products.
         </p>
 
-        <a className='self-center bg-primary text-white p-2.5 rounded flex gap-2.5 items-center text-sm' href="mailto:muhammadsubhan0712@gmail.com">Let's Connect <img src="/arrow_right_icon.svg" alt="Arrow right icon"/></a>
-     </div>
-    </section>
-  )
-}
+        <a
+          className="self-center bg-primary text-white p-2.5 rounded flex gap-2.5 items-center text-sm md:self-start md:text-xl/6"
+          href="mailto:muhammadsubhan0712@gmail.com">
+          Let's Connect{" "}
+          <img src="/arrow_right_icon.svg" alt="Arrow right icon" />
+        </a>
+      </div>
 
-export default Hero
+      <div>
+        <Image
+          src={"/profile_light_purple.png"}
+          alt="Light purple rectangle "
+          width={372}
+          height={430}
+          className="hidden md:block w-[372px] h-[430px] absolute right-0 -z-10"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
