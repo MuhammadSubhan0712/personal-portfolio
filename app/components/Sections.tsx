4c80d813328d2e55516212828d3f3eeb598fb00c
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-01-02 05:18:16 +0500

    Working..

--- app/components/Sections.tsx
+++ app/components/Sections.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Hero from './Hero/Hero';
+import Skills from './Skills/Skills';
 
 
 
@@ -7,6 +8,7 @@ const Sections = () => {
   return (
     <main className='flex flex-col gap-[142px] w-full md:max-w-secreen-lg pt-[236px] md:pt-60 mx-auto'>
     <Hero/>
+    <Skills/>
     </main>
   )
 }
