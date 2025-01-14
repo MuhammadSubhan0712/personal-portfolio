550290316a2d82f61eec6ef00d6279ea0ac3a0b0
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-01-15 02:40:33 +0500

    Working...

--- app/components/Projects/Projects.tsx
+++ app/components/Projects/Projects.tsx
@@ -1,24 +1,37 @@
-import React from 'react'
-import SectionContainer from '../Section/SectionContainer'
-import SectionHeader from '../Section/SectionHeader'
-import projects from '@/data/projects.json'
-import Project from './Project'
+import React from "react";
+import SectionContainer from "../Section/SectionContainer";
+import SectionHeader from "../Section/SectionHeader";
+import projects from "@/data/projects.json";
+import Project from "./Project";
+import Image from "next/image";
 const Projects = () => {
   return (
-    <SectionContainer id='projects'>
-      <div className='section-contents mx-6 md:mx-[64px]'>
-      <SectionHeader plainText='😎 Some of my' highlightText='Best Works'/>
-      <div className="lg:w-full grid grid-cols-1 lg:grid-cols-2 gap-10">
-        {
-          projects.map((project , id) =>
-          (<Project key={id} thumbnail={project.thumbnail} title={project.title} 
-          link={project.Link} description={project.description} languageIcons = {project.LanguageIcons}/>)
-          )
-        }
+    <SectionContainer id="projects">
+      <div className="section-contents mx-6 md:mx-[64px]">
+        <SectionHeader plainText="😎 Some of my" highlightText="Best Works" />
+        <div className="lg:w-full grid grid-cols-1 lg:grid-cols-2 gap-10">
+          {projects.map((project, id) => (
+            <Project
+              key={id}
+              thumbnail={project.thumbnail}
+              title={project.title}
+              link={project.Link}
+              description={project.description}
+              languageIcons={project.LanguageIcons}
+            />
+          ))}
+        </div>
       </div>
-      </div>
+      <Image
+        src="/projects_highlight.svg"
+        alt="Background Highlight decoration"
+        width={558}
+        height={558}
+        className="absolute hidden md:block left-1/2 top-1/2
+      -translate-x-1/2 -translate-y-1/2 -z-10"
+      />
     </SectionContainer>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
