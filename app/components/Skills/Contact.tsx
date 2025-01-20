821b34964db8d8a224a00b7e878fbeccd3fac388
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-01-21 01:37:32 +0500

    Working..

--- app/components/Skills/Contact.tsx
+++ app/components/Skills/Contact.tsx
@@ -9,7 +9,20 @@ const Contact = () => {
     md:gap-[35px] mb-[67px] md:mb-[42px">
       <div
         className="flex flex-col 
-    :md:flex-row gap-5 md:justify-between md:w-full"></div>
+    :md:flex-row gap-5 md:justify-between md:w-full">
+        <h2 className="font-semibold text-[22px] md:text-[40px] md:max-w-[462px]">
+          Want to me on your team❔ Let's make it happen🎬💫
+        </h2>
+        <div
+          className="flex flex-col gap-5
+    items-center md:items-end">
+          <a
+            href="mailto:muhammadsubhan0712@gmail.com"
+            className="self-center md:self-start bg-primary
+    text-white p-2.5 rounded flex gap-2.5 items-center text-lg
+    md:text-xl/1 font-normal"></a>
+        </div>
+      </div>
     </section>
   );
 };
