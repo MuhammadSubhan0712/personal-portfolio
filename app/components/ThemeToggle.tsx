20a2746d9ab4e62322f461f86f39b2dbcde3252a
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-01-21 02:41:58 +0500

    Working...

--- app/components/ThemeToggle.tsx
+++ app/components/ThemeToggle.tsx
@@ -1,26 +1,52 @@
-import Image from 'next/image'
-import React, { useState } from 'react'
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
 
 const ThemeToggle = () => {
-    const [isDarkMode , setIsDarkMode] = useState<boolean>(
-    typeof window !== "undefined" ? localStorage.getItem("theme")=== "dark" || 
-    window.matchMedia("(prefers-color-scheme: dark)")
-    .matches : true);
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(
+    typeof window !== "undefined"
+      ? localStorage.getItem("theme") === "dark" ||
+          window.matchMedia("(prefers-color-scheme: dark)").matches
+      : true
+  );
 
+  useEffect(() => {
+    const savedTheme =
+      typeof window !== "undefined" ? localStorage.getItem("theme") : null;
+
+    if (savedTheme) {
+      setIsDarkMode(savedTheme === "dark");
+    } else {
+      const prefersDarkMode = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      ).matches;
+      setIsDarkMode(prefersDarkMode);
+    }
+  }, []);
+
+  useEffect(() => {
+    document.body.classList.toggle("dark", isDarkMode);
+    if (typeof window !== "undefined")
+      localStorage.setItem("theme", isDarkMode ? "dark" : "light");
+  }, [isDarkMode]);
+
+  const toggletheme = () => {
+    setIsDarkMode((prev) => !prev);
+  };
   return (
-    
-    <button className="fixed top-[58px] left-6
+    <button
+      onClick={toggletheme}
+      className="fixed top-[58px] left-6
     md:top-16 md:left-auto md:right-[42px] w-9
     h-[18px] bg-[#1a1a1a] dark:bg-[#efefef]
     rounded-lg z-50 flex items-center   justify-end
     dark:justify-start px-1">
-    {isDarkMode ? <Image src="/sun_icon.svg"
-    alt="Sun icon" height={14} width={14}/> :
-    <Image src="/moon_icon.svg"
-    alt="Moon icon" height={14} width={19} />}
+      {isDarkMode ? (
+        <Image src="/sun_icon.svg" alt="Sun icon" height={14} width={14} />
+      ) : (
+        <Image src="/moon_icon.svg" alt="Moon icon" height={14} width={19} />
+      )}
     </button>
-    
-  )
-}
+  );
+};
 
-export default ThemeToggle
+export default ThemeToggle;
