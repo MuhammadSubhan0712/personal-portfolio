4fbfa665d4bb0c4557b0716a77c028490b422a74
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-01-23 09:51:28 +0500

    Working..

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,23 @@ import Image from "next/image";
 import Navbar from "./components/Navbar";
 import TopBackground from "./components/Hero/TopBackground";
 import Sections from "./components/Sections";
+import ThemeToggle from "./components/ThemeToggle";
 
 export default function Home() {
   return (
     <div className="relative overflow-clip" id="home">
-     <Sections/>
-     <Navbar/>
-     <TopBackground/>
+      <Sections />
+      <Navbar />
+      <ThemeToggle />
+      <TopBackground />
+      <Image
+        src="/botttom_gradient_mobile.svg"
+        alt="bottom gradient background"
+        height={700}
+        width={1024}
+        className="absolute bottom-0 min-w-[1024px]
+     min-h-[700px] -z-50 md:hidden"
+      />
     </div>
   );
 }
