45c70ed6e077cfdfbee2b2a9f3b19482f89a7e32
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-01-23 23:41:46 +0500

    Working...

--- app/page.tsx
+++ app/page.tsx
@@ -19,6 +19,16 @@ export default function Home() {
         className="absolute bottom-0 min-w-[1024px]
      min-h-[700px] -z-50 md:hidden"
       />
+
+      <Image
+        src="/botttom_gradient.svg"
+        alt="bott om gradient background"
+        height={936}
+        width={1557}
+        className="absolute -bottom-[175px] left-1/2 
+        -translate-x-1/2 min-w-[1557px]
+     min-h-[936px] -z-50 hidden md:block"
+      />
     </div>
   );
 }
